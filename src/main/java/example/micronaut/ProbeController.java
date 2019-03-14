75b2765930dabaae3a745e6443734955ebc3b326
antonmry <anton@galiglobal.com>
2019-03-14 11:38:41 +0100

    Some work to do

--- src/main/java/example/micronaut/ProbeController.java
+++ src/main/java/example/micronaut/ProbeController.java
@@ -15,36 +15,4 @@ public class ProbeController {
 
     static volatile State STATE = State.READY;
 
-    @Post(consumes = MediaType.APPLICATION_FORM_URLENCODED)
-    public String probeUpdate(String state) {
-
-        State prev = STATE;
-        STATE = State.valueOf(state);
-        return prev.name();
-    }
-
-    @Get("liveness")
-    public HttpResponse liveness() {
-
-        State s = STATE;
-        switch (s) {
-            case ZOMBIE:
-                return HttpResponse.serverError(s.toString());
-            default:
-                return HttpResponse.ok(State.ALIVE.toString());
-        }
-    }
-
-    @Get("readiness")
-    public HttpResponse readiness() {
-
-        State s = STATE;
-        switch (s) {
-            case READY:
-                return HttpResponse.ok(State.READY.toString());
-            default:
-                return HttpResponse.serverError(s.toString());
-        }
-    }
-
 }
